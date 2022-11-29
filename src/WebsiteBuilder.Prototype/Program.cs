ee5b4de8e23caaae662409695530d47a2c907bdb
Chad Green <chadgreen@chadgreen.com>
2022-11-29 00:22:10 -0500

    Building the speaking enagement pages

--- src/WebsiteBuilder.Prototype/Program.cs
+++ src/WebsiteBuilder.Prototype/Program.cs
@@ -4,9 +4,12 @@ using WebsiteBuilder.Prototype.Data;
 using WebsiteBuilder.Prototype.Models;
 using WebsiteBuilder.Prototype.Services;
 
+const string outputPath = @"D:\Temp\";
 const string templatePath = @"D:\Repos\TaleLearnCode\WebsiteBuilder\src\WebsiteBuilder.Prototype\PageTemplates\";
 const string presentationDetailTemplatePath = $"{templatePath}template_presentation.html";
 const string presenationListTemplatePath = $"{templatePath}template_presentations.html";
+const string speakingEngaementListTemplatePath = $"{templatePath}template_speaking-engagements.html";
+const string speakingEngaementDetailTemplatePath = $"{templatePath}template_speaking-engagement.html";
 
 using WebsiteBuilderContext websiteBuilderContext = new WebsiteBuilderContext();
 
@@ -29,11 +32,37 @@ List<Presentation> presentations = await websiteBuilderContext.Presentations
 		.ThenInclude(x => x.ShindigPresentationDownloads)
 	.ToListAsync();
 
-using ProgressBar progressBar = new(presentations.Count, $"Building static pages (1 of {presentations.Count + 1})");
-PresentationServices presentationServices = new(upcomingShindigs, @"D:\Temp\");
+Console.WriteLine("Retrieving list of speaking engagements...");
+List<Shindig> speakingEngagements = await websiteBuilderContext.Shindigs
+	.Include(x => x.ShindigPresentations)
+		.ThenInclude(x => x.ShindigPresentationDownloads)
+	.ToListAsync();
+
+using ProgressBar progressBar = new(presentations.Count + 2 + speakingEngagements.Count, $"Building static pages (1 of {presentations.Count + 2})");
+
+PresentationServices presentationServices = new(upcomingShindigs, outputPath);
 await presentationServices.BuildPresentationListPageAsync(presentations, presenationListTemplatePath);
+ProgressBarTick();
 foreach (Presentation presentation in presentations)
 {
 	await presentationServices.BuildPresentationPageAsync(presentation, presentationDetailTemplatePath);
-	progressBar.Tick($"Building presentation pages ({progressBar.CurrentTick + 1} of {progressBar.MaxTicks})");
+	ProgressBarTick();
 }
+
+SpeakingEngagementServices speakingEngagementServices = new(upcomingShindigs, outputPath);
+List<Shindig> shindigs = await websiteBuilderContext.Shindigs
+	.OrderByDescending(x => x.StartDate)
+	.ToListAsync();
+await speakingEngagementServices.BuildShindigListingPageAsync(shindigs, speakingEngaementListTemplatePath);
+ProgressBarTick();
+
+foreach (Shindig speakingEngagement in speakingEngagements)
+{
+	await speakingEngagementServices.BuildShiningDetailPageAsync(speakingEngagement, speakingEngaementDetailTemplatePath);
+	ProgressBarTick();
+}
+
+
+
+void ProgressBarTick() =>
+	progressBar.Tick($"Building static pages ({progressBar.CurrentTick + 1} of {progressBar.MaxTicks})");
